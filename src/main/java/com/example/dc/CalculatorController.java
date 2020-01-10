dbce21373273de8f806ea0069a74da0b5ba46f01
Emily Jiang <emijiang6@googlemail.com>
2020-01-10 10:33:23 +0000

    simplify the code

--- src/main/java/com/example/dc/CalculatorController.java
+++ src/main/java/com/example/dc/CalculatorController.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.inject.Singleton;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,11 +18,10 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @ApplicationScoped
 public class CalculatorController {
 
-    double discount = 0.1;
-
+    @Inject @ConfigProperty(name="discount", defaultValue = "0.2") double discount;
     @GET
     @Path("/{price}")
     public String getPrice(@PathParam("price") int price) {
-        return String.valueOf(new DecimalFormat("##.##").format((price * (1 - discount))));
+        return "Original Price: " + price + "   You Pay: " + String.valueOf(new DecimalFormat("##.##").format((price * (1 - discount))));
     }
 } 
